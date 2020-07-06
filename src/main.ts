37bde63713df2d1d56627e61ea89867ca69ab6ea
HopeBing <1259713996@qq.com>
2020-07-06 21:53:59 +0800

    前端渲染

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import * as path from 'path';
 import * as nunjucks from 'nunjucks';
+import { blue } from 'colors';
+import { address } from 'ip';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -14,7 +16,15 @@ async function bootstrap() {
         autoescape: true,
         express: app,
     });
-  await app.listen(8099);
+
+  const port = 8109;
+  await app.listen(port, () => {
+    console.log(
+      blue(
+        `当前服务运行在 \n http://localhost:${port} \n http://${address()}:${port}`,
+      ),
+    );
+  });
 }
 
 
